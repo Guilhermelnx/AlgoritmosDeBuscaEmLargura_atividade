424ba9d8225b76591334b9aece1dbc8be81b0424
Guilherme Conceição <lnx1971@gmail.com>
2026-09-09 09:53:03 -0300

    Update initial and goal states in Main.java

--- Aplicativo_1/src/Main.java
+++ Aplicativo_1/src/Main.java
@@ -6,11 +6,11 @@ public class Main {
         // 1 2 3
         // 5 6 0
         // 7 8 4
-        String estadoInicial = "123560784";
+        String estadoInicial = "724506831";
 
         // estado objetivo organizado: 123456780
 
-        String estadoObjetivo = "123456780";
+        String estadoObjetivo = "012345678";
 
         System.out.println("Buscando a solução mais curta...");
         int movimentos = Quebra_cabeca_Deslizante.buscarEmLargura(estadoInicial, estadoObjetivo);
