08b7cdd35e84e2d5dfe9f972ef86dd6730a8c260
Guilherme Conceição <lnx1971@gmail.com>
2026-09-09 14:57:42 -0300

    Rename Main class to RotaViagem

--- RotaViagem.java
+++ RotaViagem.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class RotaViagem {
     public static void main(String[] args) {
         String[] cidades = {
             "Aparecida de Goiânia",
